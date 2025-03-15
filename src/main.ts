7607ab3d49b095132a8c65fcf76f8ba76c7f4ee1
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-03-15 15:25:12 +0000

    add many answers, some of which might be correct
    Co-authored-by: HOE18HarrisJ <HOE18HarrisJ@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,89 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css"
+const inputArea = document.querySelector<HTMLTextAreaElement>("#input")!
+const calculateButton = document.querySelector<HTMLButtonElement>(".calculate")!
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+async function answerImage(prompt: string) {
+  const body = new FormData()
+  body.append("text", prompt)
+  body.append("image_generator_version", "standard")
+  body.append("use_old_model", "false")
+  body.append("turbo", "true")
+  body.append("genius_preference", "classic")
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const res = await fetch("https://api.deepai.org/api/text2img", {
+    method: "POST",
+    body,
+    headers: {
+      "Api-Key": "tryit-42662075430-0e15226c78b8d0a7f6da159148ca9f08",
+    },
+  })
+
+  const json = await res.json()
+  alert(json.output_url)
+}
+
+async function solve(expression: string): Promise<string> {
+  const textPrompt = `You are a calculator that takes an expression and gives a simpler expression.
+  
+  Follow these steps with the provided input:
+  1. Come up with a numerical answer to the maths problem
+  2. Then, take the numerical answer, and create an equation that's equivalent to the answer, e.g. for 11, generate 2 * 5 + 1
+  3. Try to include fractions, roots, and exponents
+
+  Put the final output into pipe tags, e.g. <|>2 * 5 + 1</|>
+
+  Input: <input>${expression}</value>
+  `
+
+  const requestBody = {
+    contents: [{ parts: [{ text: textPrompt }] }],
+  }
+  const res = await fetch("https://calculate-her.mmk21-spam.workers.dev/", {
+    method: "POST",
+    body: JSON.stringify(requestBody),
+  })
+  const data = await res.json()
+  const response: string = data.candidates[0].content.parts[0].text
+
+  const finalOutputRegex = /<\|>(.*)<\/\|>/gm
+  const match = finalOutputRegex.exec(response)
+  if (!match) {
+    alert("Invalid response")
+    throw Error("Invalid response")
+  }
+  const finalOutput = match[1]
+  return finalOutput
+}
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max)
+}
+
+calculateButton.addEventListener("click", async () => {
+  const givenInput = inputArea.value
+
+  if (!givenInput) {
+    while (confirm("ENTER SOME INPUT YOU FOOL!")) {}
+    return
+  }
+
+  const actualAnswer = await solve(givenInput)
+  const allAnswers = await Promise.all([
+    solve((getRandomInt(1000) - 500).toString()),
+    actualAnswer,
+    solve((getRandomInt(1000) - 500).toString()),
+    solve((getRandomInt(1000) - 500).toString()),
+    solve((getRandomInt(1000) - 500).toString()),
+  ])
+
+  const answerDivs = allAnswers.map((answer) => {
+    const div = document.createElement("div")
+    div.innerText = answer
+    div.className = "answer"
+    return div
+  })
+
+  document.querySelector(".answers")?.append(...answerDivs)
+
+  document.querySelector(".answer")!.textContent = actualAnswer
+})
